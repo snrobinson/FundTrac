d906972a0976e7440cc2db79b2387cb9e1262f2e
snrobinson <snrobinson94@gmail.com>
2019-04-11 16:01:22 -0400

    FundTrac Update 4/11/19

--- FundTrac/JS/app.js
+++ FundTrac/JS/app.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 /* eslint-disable no-unused-vars */
 /*jshint multistr: true */
 
@@ -6,13 +7,17 @@ var bAmount = '-';
 var bDate = '-';
 var objBills;
 var billArray = [];
+var drpVal = document.querySelector('.datalistIDs').value;
 
-function displayBillTable () {
+// Displays Bills html table
+function displayBillTable() {
 	var msgHead = '';
 	var msgBody = '';
 	var msgFoot = '';
 	var msg = '';
-	msgHead = '<table  class="billsTable" style="border-style: solid;"><thead><tr><th colspan="4">Bills</th></tr><tr>\
+	var i;
+	var l = i + 1;
+	msgHead = '<table  id="billsTable"><thead><tr><th colspan="4">Bills</th></tr><tr>\
                         <th>ID</th>\
                         <th>Name</th>\
                         <th>Payment Amount</th>\
@@ -20,11 +25,11 @@ function displayBillTable () {
 
 	msgBody = msgBody + '<tbody><tr>';
 
-	for (var i = 0; i < billArray.length; i++) {
+	for (i = 0; i < billArray.length; i++) {
 		msgBody = msgBody + '<td>' + i + '</td>\
     <td>' + billArray[i].name + '</td>\
     <td>' + billArray[i].amount + '</td>\
-		<td>' + billArray[i].date + '</td></tr>';
+	<td>' + billArray[i].date + '</td></tr>';
 	}
 	msgBody = msgBody + '</tbody>';
 
@@ -32,20 +37,24 @@ function displayBillTable () {
 	msgFoot = '</table>';
 	// BUILD TABLE
 	msg = msgHead + msgBody + msgFoot;
-	// alert("The bills button works!")
 
 	document.querySelector('.billsTable').innerHTML = msg;
+
+	document.querySelector('.datalistIDs').innerHTML = '<option value="blank"></option>';
+	billArray.forEach(function (n, x) {
+		document.querySelector('.datalistIDs').insertAdjacentHTML('beforeend', '<option value="' + x + '">' + x + '</option>');
+	});
+
 }
 
-function writeToBillTable (name, amount, dueDate) {
+// Writes user input to Bills array
+function writeToBillTable(name, amount, dueDate) {
 	var success = true;
 	// Create array to hold bill objects
 	// Create Bill object
 
 	try {
-		billArray.push({name: name, amount: amount, date: dueDate});
-
-		// eslint-disable-next-line no-console
+		billArray.push({ name: name, amount: amount, date: dueDate });
 		console.log(billArray[0].name);
 	} catch (ex) {
 		success = false;
@@ -53,25 +62,33 @@ function writeToBillTable (name, amount, dueDate) {
 
 	return success;
 }
+
+// Bills button
 document.querySelector('.bBills').addEventListener('click', function () {
 	document.querySelector('.bNewBill').style.display = 'inline-block';
-	document.querySelector('.bUpdateBill').style.display = 'inline-block';
 	document.querySelector('.billName').style.display = 'inline-block';
 	displayBillTable();
+	if (!billArray[0].name) {
+		document.querySelector('.bUpdateBill').style.display = 'none';
+	} else {
+		document.querySelector('.bUpdateBill').style.display = 'inline-block';
+	}
 });
 
-document.querySelector('.bNewBill').addEventListener('click', function(){
+// New button
+document.querySelector('.bNewBill').addEventListener('click', function () {
 	document.querySelector('.bNewBill').style.display = 'none';
 	document.querySelector('.bUpdateBill').style.display = 'none';
 	document.querySelector('.bAddNewBill').style.display = 'inline-block';
 	document.querySelector('.inputsBill').style.display = 'inline-block';
+	document.querySelector('.bCancel').style.display = 'inline-block';
+
+	document.querySelector('.billAmount').value = null;
+	document.querySelector('.billName').value = null;
+	document.querySelector('.billDate').value = null;
 });
 
-// document.querySelector('.bBills').addEventListener('click', displayBillInput)
-document.querySelector('.bAddNewBill').addEventListener('click', function () { 
-	document.querySelector('.billName').style.display = 'inline-block';
-});
-
+// Add button
 document.querySelector('.bAddNewBill').addEventListener('click', function () {
 	var amount = document.querySelector('.billAmount').value;
 	var name = document.querySelector('.billName').value;
@@ -81,53 +98,66 @@ document.querySelector('.bAddNewBill').addEventListener('click', function () {
 	document.querySelector('.bNewBill').style.display = 'inline-block';
 	document.querySelector('.bUpdateBill').style.display = 'inline-block';
 	document.querySelector('.bAddNewBill').style.display = 'none';
+	document.querySelector('.bCancel').style.display = 'none';
 	displayBillTable();
 });
 
+// Update button
 document.querySelector('.bUpdateBill').addEventListener('click', function () {
+	document.querySelector('.datalistIDs').style.display = 'inline-block';
+	document.querySelector('.inputsBill').style.display = 'none';
+	document.querySelector('.bNewBill').style.display = 'none';
+	document.querySelector('.bUpdateBill').style.display = 'none';
+	document.querySelector('.bCancel').style.display = 'inline-block';
+	displayBillTable();
+});
+
+// Cancel button
+document.querySelector('.bCancel').addEventListener('click', function(){
+	document.querySelector('.bNewBill').style.display = 'inline-block';
+	document.querySelector('.bAddNewBill').style.display = 'none';
+	document.querySelector('.inputsBill').style.display = 'none';
+	document.querySelector('.bCancel').style.display = 'none';
+	document.querySelector('.datalistIDs').style.display = 'none';
+	document.querySelector('.bSaveBill').style.display = 'none';
+	if (!billArray[0].name) {
+		document.querySelector('.bUpdateBill').style.display = 'none';
+	} else {
+		document.querySelector('.bUpdateBill').style.display = 'inline-block';
+	}
+});
+
+// Drop down update
+document.querySelector('.datalistIDs').addEventListener('change', function(e){
+	document.querySelector('.bNewBill').style.display = 'none';
+	document.querySelector('.inputsBill').style.display = 'inline-block';
+	document.querySelector('.bSaveBill').style.display = 'inline-block';
+	document.querySelector('.bCancel').style.display = 'inline-block';
+	document.querySelector('.billAmount').value = billArray[e.target.value].amount;
+	document.querySelector('.billName').value = billArray[e.target.value].name;
+	document.querySelector('.billDate').value = billArray[e.target.value].date;
+});
+
+// Save bill after updating
+document.querySelector('.bSaveBill').addEventListener('click', function(){
+	var drpIndex = document.querySelector('.datalistIDs');
+	var index = drpIndex.options[drpIndex.selectedIndex].value;
 	var amount = document.querySelector('.billAmount').value;
 	var name = document.querySelector('.billName').value;
 	var dueDate = document.querySelector('.billDate').value;
-	writeToBillTable(name, amount, dueDate);
 	document.querySelector('.inputsBill').style.display = 'none';
+	document.querySelector('.bSaveBill').style.display = 'none';
+	document.querySelector('.datalistIDs').style.display = 'none';
+	document.querySelector('.bCancel').style.display = 'none';
 	document.querySelector('.bNewBill').style.display = 'inline-block';
-	document.querySelector('.bUpdateBill').style.display = 'inline-block';
-	document.querySelector('.bAddNewBill').style.display = 'none';
+	if (!billArray[0].name) {
+		document.querySelector('.bUpdateBill').style.display = 'none';
+	} else {
+		document.querySelector('.bUpdateBill').style.display = 'inline-block';
+	}
+	billArray[index].name = name;
+	billArray[index].amount = amount;
+	billArray[index].date = dueDate;
 	displayBillTable();
-});
-
-// function inputBill (bname, bamount, bdate) {
-// 	var billName = ''
-// 	var billAmount = 0
-// 	var billDate = ''
-// 	// var billFormattedDate = new Date(date)
-
-// 	var newBill = new objBills
-// 	newBill.name = 'Stephen'
-// 	billArray.push(x)
-// 	// billList[i] = [name, amount, billFormattedDate]
-// 	console.log(billArray[0])
-// // return 
-// }
-
-// function displayBillInput () {
-// 	var msg = '';
-// 	var msgBody = '';
-// 	var msgHead = '';
-// 	var msgFoot = '';
-
-// 	msg = '<input type="text" name="billName" class="billName" placeholder="Name of bill"/>\
-//     <input type="number" name="billAmount" class="billAmount" placeholder="Amount due"/>\
-//     <input type="date" name="billDate" class="billDate" placeholder="Due date"/>';
-
-// 	document.querySelector('.billInput').insertAdjacentHTML('afterbegin', msg);
-// }
-
-// document.querySelector('.billName').oninput = function(){
-//   new objBills.name = document.querySelector('.billName').value
-// }
-
-// bAmount = document.querySelector('.billAmount').value
-// bDate = document.querySelector('.billName').value
-
+}); 
 
